3989b53d0a2e7fff741aea22a4a42b8aad9c7ef0
Ace <41267713+Niobox@users.noreply.github.com>
2021-10-06 21:07:20 +0800

    1.1

---
+++ assets/js/myjs.js
@@ -0,0 +1,13 @@
+var OriginTitile = document.title;
+				var titleTime;
+				document.addEventListener('visibilitychange', function() {
+					if (document.hidden) {
+						document.title = '隐秘籍——土遁';
+						clearTimeout(titleTime);
+					} else {
+						document.title = '阿嘞嘞～～';
+						titleTime = setTimeout(function() {
+							document.title = OriginTitile;
+						}, 2000);
+					}
+				});
