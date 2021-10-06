6aa04ee60d7aa726df06145a2fbbec5e9336bca5
Ace <41267713+Niobox@users.noreply.github.com>
2021-10-06 21:15:26 +0800

    1

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,13 +1,14 @@
+// title脚本
 var OriginTitile = document.title;
-				var titleTime;
-				document.addEventListener('visibilitychange', function() {
-					if (document.hidden) {
-						document.title = '隐秘籍——土遁';
-						clearTimeout(titleTime);
-					} else {
-						document.title = '阿嘞嘞～～';
-						titleTime = setTimeout(function() {
-							document.title = OriginTitile;
-						}, 2000);
-					}
-				});
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+        document.title = '隐秘籍——土遁';
+        clearTimeout(titleTime);
+    } else {
+        document.title = '阿嘞嘞～～';
+        titleTime = setTimeout(function() {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
